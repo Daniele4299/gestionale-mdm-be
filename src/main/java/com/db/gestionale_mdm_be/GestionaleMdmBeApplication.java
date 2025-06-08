0a735f7960110c9954152852c1cc6e5dffaeccf8
Daniele4299 <bonadonnadaniele@gmail.com>
2025-06-08 21:25:48 +0200

    First commit

---
+++ src/main/java/com/db/gestionale_mdm_be/GestionaleMdmBeApplication.java
@@ -0,0 +1,13 @@
+package com.db.gestionale_mdm_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleMdmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleMdmBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/gestionale_mdm_be/GestionaleMdmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.gestionale_mdm_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleMdmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
